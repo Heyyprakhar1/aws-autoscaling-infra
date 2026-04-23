62cd44dc2a88760d3368770bc0c4371fa90df549
Heyyprakhar1 <srivprak0106@gmail.com>
2026-04-23 09:29:06 +0000

    modifed terraform.tf in accordance to remote-backend

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "6.41.0"
     }
   }
+  backend "s3" {
+    bucket = "aws-autoscaling-infra-bucket"
+    dynamodb_table = "aws-autoscaling-infra-lock-table"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+    }
 }
